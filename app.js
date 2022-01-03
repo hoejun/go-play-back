7bbdbf437ba5b2d5a33539639ccd0e7beb50c009
kimhoejun <hjkim.it@gmail.com>
2022-01-04 00:40:36 +0900

    no message

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 //익스프레스
 var app = express();
-var app = express();
   
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
